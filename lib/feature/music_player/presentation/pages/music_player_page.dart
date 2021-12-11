b9ba92679414cd74c683765f7dbaaa110611ae8a
muhibush <ukiacrew@gmail.com>
2021-12-11 21:59:14 +0700

    feat: simple play & pause music

--- lib/feature/music_player/presentation/pages/music_player_page.dart
+++ lib/feature/music_player/presentation/pages/music_player_page.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:music_player/feature/music_player/presentation/bloc/music_bloc.dart';
 import 'package:music_player/feature/music_player/presentation/widgets/empty_state.dart';
 import 'package:music_player/feature/music_player/presentation/widgets/music_item.dart';
 import 'package:provider/provider.dart';
 
-class MusicPlayerPage extends StatelessWidget {
+class MusicPlayerPage extends StatefulWidget {
   const MusicPlayerPage({Key? key}) : super(key: key);
+
+  @override
+  State<MusicPlayerPage> createState() => _MusicPlayerPageState();
+}
+
+class _MusicPlayerPageState extends State<MusicPlayerPage> {
+  late AudioPlayer _player;
+
+  @override
+  void initState() {
+    super.initState();
+    _player = AudioPlayer();
+  }
+
+  @override
+  void dispose() {
+    _player.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,8 +52,16 @@ class MusicPlayerPage extends StatelessWidget {
                 var music = listMusic[index];
                 return MusicItem(
                   music: music,
-                  onTap: () {
-                    print(music.trackName);
+                  onTap: () async {
+                    var url = music.previewUrl;
+                    var isPlaying = _player.playing;
+
+                    if (isPlaying) {
+                      _player.pause();
+                    } else if (url != null) {
+                      await _player.setUrl(url);
+                      _player.play();
+                    }
                   },
                 );
               });
